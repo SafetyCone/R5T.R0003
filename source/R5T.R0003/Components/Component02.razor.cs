ea0c9e351de92f3d924f3b5bda0ada3c5bf500a8
davidcoats <david@getsafetycone.com>
2023-01-25 20:20:54 +0530

    Interim changes.

--- source/R5T.R0003/Components/Component02.razor.cs
+++ source/R5T.R0003/Components/Component02.razor.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 
+using R5T.T0154;
+
 
 namespace R5T.R0003
 {
     /// <summary>
     /// Here is some documentation.
     /// </summary>
+    [RazorComponentMarker]
     public partial class Component02
     {
     }
